0551d82bba7c66326427ecc0471cea81eb0382d1
mm_dev <bowwck@gmail.com>
2019-06-10 15:23:51 +0300

    update

--- server.js
+++ server.js
@@ -5,12 +5,9 @@
   var bodyParser = require('body-parser');
   var app = express();
 
-app.set('port', process.env.PORT || 8080);
-app.use(express.static(__dirname + '/app'));
-app.use(bodyParser.json());
+  app.set('port', process.env.PORT || 8080);
+  app.use(express.static(__dirname + '/app'));
+  app.use(bodyParser.json());
 
-http.createServer(app).listen(app.get('port'), function(){
-console.log("Express server listening on port " + app.get('port'));
-  });       
-
+  http.createServer(app).listen(app.get('port'), function(){});
 }).call(this);
