bb573dccb36a9aaec499161992abd2e8427db3de
mm_dev <bowwck@gmail.com>
2019-07-07 20:11:04 +0300

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
   var bodyParser = require('body-parser');
   var app = express();
 
-  app.set('port', 8080);
+  app.set('port', 8000);
   app.use(express.static(__dirname + '/app'));
   app.use(bodyParser.json());
 
